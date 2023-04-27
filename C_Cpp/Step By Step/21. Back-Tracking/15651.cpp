352a078767039d065a0eabfbef8340f538e6d467
Lulzhub <88299137+JngVedere@users.noreply.github.com>
2023-04-27 22:50:44 +0900

    .
    .

--- C_Cpp/Step By Step/21. Back-Tracking/15651.cpp
+++ C_Cpp/Step By Step/21. Back-Tracking/15651.cpp
@@ -1,17 +1,17 @@
-//BOJ 15651 - SILVER 3
-//https://www.acmicpc.net/problem/15651
+//BOJ 15652 - SILVER 3
+//https://www.acmicpc.net/problem/15652
 
 #include <iostream>
 using namespace std;
 int N,M;
 
-int checks[8] = {0, }, nums[8] = {0, };
+int checks[9] = {0, }, nums[9] = {0, };
 
 void backTrack(int cnt = 0){
     if(cnt == M){
         for(int i = 0; i<M; i++) cout << nums[i] << ' ';
         cout << "\n";
-    }else for(int i = 1; i <= N; i++){ //dfs part
+    }else for(int i = 1; i <= N; i++) if(cnt==0 || nums[cnt-1] <= i){ //dfs part
         checks[i] = 1;
         nums[cnt] = i;
         backTrack(cnt+1);
