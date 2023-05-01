4b019e91197f5d3586f0e7b5e7024329b6272b43
Lulzhub <88299137+JngVedere@users.noreply.github.com>
2023-05-01 22:45:48 +0900

    .
    .

--- C_Cpp/Step By Step/21. Back-Tracking/9663.cpp
+++ C_Cpp/Step By Step/21. Back-Tracking/9663.cpp
@@ -1,48 +1,43 @@
 //BOJ 9663 - GOLD 4
 //https://www.acmicpc.net/problem/9663
 #include <iostream>
-using namespace std;
-bool chess[16][16] = {0,};
-int n, cnt = 0, res = 0; // 1 <= N < 15
+#include <cmath>
 
-bool isPlacable(int x, int y){
-    for(int i = 0; i < n; i++){
-        if(chess[x][i] || chess[i][y]) return false;
-        if (x - i >= 0 && y - i >= 0 && chess[x - i][y - i]) return false;
-		if (x + i < n && y + i < n && chess[x + i][y + i]) return false;
-		if (x - i >= 0 && y + i < n && chess[x - i][y + i]) return false;
-		if (x + i < n && y - i >= 0 && chess[x + i][y - i]) return false;
-	}
-    return true;
+int chess[15];
+int n;
+int res = 0; // 1 <= N < 15
+
+int promising(int col){
+    
+    for(int i = 0; i < col; i++){
+        if(chess[col] == chess[i] || col - i == abs(chess[col] - chess[i])){
+            
+            return 0;
+        }
+    }
+        
+    return 1;
 } 
 
-void backTrack(int cnt){
+void nqueen(int cnt){
     if(cnt == n){
-        // for(int i = 0; i < n; i++){
-        //     for(int j = 0; j < n; j++) 
-        //         cout << (chess[i][j] ? "1 " : "0 ");
-        //     cout << "\n";
-        // }
-        // cout << "\n";
         res++;
         return;
     }
 
     for(int i = 0; i < n; i++){
-        if(!isPlacable(cnt,i)) continue;
+        chess[cnt] = i;
 
-        chess[cnt][i] = 1;
-        backTrack(cnt+1);
-        chess[cnt][i] = 0;
+        if(promising(cnt)){
+            nqueen(cnt+1);
+        }
     }
 }
 
 int main(){
-    ios :: sync_with_stdio(false);
-    cin.tie(NULL);
+    scanf("%d",&n);
+    nqueen(0);
+    printf("%d",res);
 
-    cin >> n;
-    backTrack(0); 
-    cout << res;
     return 0;
 }
