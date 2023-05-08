bba5212d2a31b69b629ad4154ca286a2727d6367
Lulzhub <88299137+JngVedere@users.noreply.github.com>
2023-05-08 23:08:17 +0900

    .
    .

---
+++ C_Cpp/2441.cpp
@@ -0,0 +1,21 @@
+//BOJ 2441 - BRONZ 3
+//https://www.acmicpc.net/problem/2441
+#include <iostream>
+using namespace std;
+int S;
+
+int main(){
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> S;
+
+    for(int i = S; i > 0; i--){
+        for(int j = 0; j < S - i;j++){
+            cout << " ";
+        }
+        for(int k = 0; k < i; k++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
