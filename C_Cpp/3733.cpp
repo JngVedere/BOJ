0c252905405c068061469caa5fc9bfc5f8ca35f6
Lulzhub <88299137+JngVedere@users.noreply.github.com>
2023-05-09 22:12:22 +0900

    .
    .

---
+++ C_Cpp/3733.cpp
@@ -0,0 +1,14 @@
+//BOJ 3833 - BRONZ 5
+//https://www.acmicpc.net/problem/3733
+#include <iostream>
+using namespace std;
+int N, S, x;
+
+int main(){
+
+    while(scanf("%d%d",&N,&S)==2){
+        x = S/(N+1);
+        printf("%d\n",x);
+    }
+    return 0;
+}
