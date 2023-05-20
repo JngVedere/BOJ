1d9c7ef95cc8090300b0fe82364d4bffb04f9f12
Lulzhub <88299137+JngVedere@users.noreply.github.com>
2023-05-20 23:44:44 +0900

    .
    .

--- C_Cpp/Step By Step/22. DP 1/9461.cpp
+++ C_Cpp/Step By Step/22. DP 1/9461.cpp
@@ -2,10 +2,18 @@
 //https://www.acmicpc.net/problem/9461
 #include <iostream>
 using namespace std;
+int T, n;
+long long pdv[101];
 
 int main(){
     ios :: sync_with_stdio(false);
     cin.tie(NULL);
-
+    cin >> T;
+    pdv[1] = pdv[2] = pdv[3] = 1;
+    for(int i = 4; i <= 100; i++) pdv[i] = pdv[i-2] + pdv[i-3];
+    while(T--){
+        cin >> n;
+        cout << pdv[n] << "\n";
+    }
     return 0;
 }
