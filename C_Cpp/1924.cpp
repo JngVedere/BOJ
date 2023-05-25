fc0fe3afdc953ebbd939c5f392a7e61730b60e44
Lulzhub <88299137+JngVedere@users.noreply.github.com>
2023-05-25 22:51:44 +0900

    .
    .

---
+++ C_Cpp/1924.cpp
@@ -0,0 +1,16 @@
+//BOJ 1924 - BRONZ 1
+//https://www.acmicpc.net/problem/1924
+#include <iostream>
+using namespace std;
+int x,y,res;
+int num[12] = {6,2,2,5,0,3,5,1,4,6,2,4};
+string days[7] = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
+
+int main(){
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> x >> y;
+    res = (1 + num[x-1] + y) % 7;
+    cout << days[res];
+    return 0;
+}
