98f6c3973fc1c56480e76b4aa085964060a4e283
Lulzhub <88299137+JngVedere@users.noreply.github.com>
2023-05-31 22:46:24 +0900

    .
    .

---
+++ C_Cpp/2440.cpp
@@ -0,0 +1,12 @@
+//BOJ 2440 - BRONZ 4
+//https://www.acmicpc.net/problem/2440
+#include <iostream>
+using namespace std;
+int t;
+int main(){
+    cin >> t;
+    for(int i = t; i > 0; i--){
+        for(int j = 0; j < i; j++) cout << "*";
+        cout << "\n";
+    }
+}
---
+++ C_Cpp/2531.cpp
@@ -0,0 +1,11 @@
+//BOJ 2531 - SILVER 1
+//https://www.acmicpc.net/problem/2531
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
