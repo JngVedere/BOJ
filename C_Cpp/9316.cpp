e774375a4bdd3830c137c48792b6a261a9371cab
Lulzhub <88299137+JngVedere@users.noreply.github.com>
2023-06-22 20:41:57 +0900

    .
    .

---
+++ C_Cpp/9316.cpp
@@ -0,0 +1,12 @@
+//BOJ 9316 - BRONZ 4
+//https://www.acmicpc.net/problem/9316
+#include <iostream>
+using namespace std;
+int n;
+
+int main(){
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n;
+    for(int i = 1; i <= n; i++) cout << "Hello World, Judge " << i << "!\n";
+}
